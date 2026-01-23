84472189b99bd6a13ec47290e2c7161611cc5a04
EJ-hiramatsu <ryuki_hiramatsu@esrij.com>
2026-01-23 15:55:55 +0900

    2026/1/23 PM 時点 UI 以外完成

--- samples/gia/script/common/ui.js
+++ samples/gia/script/common/ui.js
@@ -172,6 +172,12 @@ function changeMapMode() {
     const iconDiv = document.getElementById("layer-icon");
     const nonLyrCrd = document.getElementById("non-layer-card");
     const layerView = document.getElementById("layer-list");
+    const layerFlow = document.querySelector(`[data-flow-id="layer"]`);
+    console.log("layerFlow.children", layerFlow.children)
+    if (layerFlow.children.length > 1) {
+        layerFlow.back();
+        layerFlow.lastElementChild.remove();
+    }
     if (viewEl.map.layers.length > 0) {
         iconDiv.style.display = "none";
         nonLyrCrd.style.display = "none";
